92a52d73c07922fd79354dc626d87b00d71b18bb
Niklas Mollenhauer <nikeee@outlook.com>
2016-10-24 20:57:45 +0200

    Resolve #9

--- src/flipdot.ts
+++ src/flipdot.ts
@@ -1,4 +1,4 @@
-import * as request from "request";
+import fetch, { Headers, Request, RequestInit, Response } from "node-fetch";
 
 const spaceStatusURL = "http://flipdot.org/spacestatus/status.json";
 const powerConsumptionURL = "http://infragelb.de/flipdot-power/";
@@ -23,7 +23,6 @@ export interface SpaceStatus {
 export interface PowerConsumption {
 	/**
 	 * Power consumption in Watts
-	 * @type {number}
 	 */
 	consumption: number;
 	timestamp: Date;
@@ -46,74 +45,38 @@ export const enum LightStatus {
 	On = 1
 }
 
-type RequestFunction = (url: string, callback: (err: any, res: any, body: any) => void) => void;
-type ParseFunction = (body: string) => void;
-type Parser<T> = ((body: string) => [any | null, T | null]);
-
-function wrapWithTry<T>(fn: () => T): [Error | null, T | null] {
-	try {
-		return [null, fn()];
-	} catch (ex) {
-		return [ex, null];
-	}
-}
-
-function doAndParseRequest<T>(request: RequestFunction, parser: Parser<T> | null, url: string): Promise<T | null> {
-	return new Promise<T | null>((resolve, reject) => {
-		let hadError = false;
-		request(url, (err, res, body) => {
-			if (!err && isSuccess(res)) {
-				if (hadError) // If request calls the callback although it already reported an error
-					return; // avoid calling the callback twice.
-
-				let [err, finalResult] = parser !== null ? parser(body) : [null, null];
-				return err ? reject(err) : resolve(finalResult);
-			}
-			if (!!err) {
-				hadError = true;
-				reject(err);
-			}
-		});
-	});
-}
-
 /**
  * Switches the orange light on or off.
  * @deprecated The light does not exist any more.
  */
 export function setOrangeLightStatus(status: LightStatus): Promise<void> {
-	let statusString = status == LightStatus.On ? "true" : "false";
-	let orangeLightUrl = getCANUrl(hutshieneClientName, "OrangeLight");
-	let statusUrl = `${orangeLightUrl}?state=${statusString}`;
+	const statusString = status == LightStatus.On ? "true" : "false";
+	const orangeLightUrl = getCANUrl(hutshieneClientName, "OrangeLight");
+	const statusUrl = `${orangeLightUrl}?state=${statusString}`;
 
-	return doAndParseRequest<void>(
-		request.post,
-		null,
-		statusUrl);
+	return fetch(statusUrl, { method: "POST" });
 }
 
 /**
  * Gets the current temperature as measured by the sensor of the radiator control.
  */
-export function getCurrentTemperature(): Promise<Temperature> {
+export async function getCurrentTemperature(): Promise<Temperature> {
 	let serviceUrl = getCANUrl(radiatorClientName, "GetActTemp");
-
-	return doAndParseRequest<Temperature>(
-		request.get,
-		body => wrapWithTry(() => parseTemperature(body)),
-		serviceUrl);
+	const res = await fetch(serviceUrl);
+	throwIfNotOkay(res);
+	const currentTemp = await res.text();
+	return parseTemperature(currentTemp);
 }
 
 /**
  * Gets the temperature that the radiator is set to.
  */
-export function getTargetTemperature(): Promise<Temperature> {
-	let serviceUrl = getCANUrl(radiatorClientName, "GetTargetTemp");
-
-	return doAndParseRequest<Temperature>(
-		request.get,
-		body => wrapWithTry(() => parseTemperature(body)),
-		serviceUrl);
+export async function getTargetTemperature(): Promise<Temperature> {
+	const serviceUrl = getCANUrl(radiatorClientName, "GetTargetTemp");
+	const res = await fetch(serviceUrl);
+	throwIfNotOkay(res);
+	const targetTemp = await res.text();
+	return parseTemperature(targetTemp);
 }
 
 /**
@@ -123,42 +86,35 @@ export function getTargetTemperature(): Promise<Temperature> {
 export function setTargetTemperature(temperature: number): Promise<void> {
 	// TODO: TEST THIS
 	let opUrl = getCANUrl(radiatorClientName, "SetTargetTemp");
-	let serviceUrl = `${opUrl}?temp=${temperature}`;
+	const serviceUrl = `${opUrl}?temp=${temperature}`;
 
-	return doAndParseRequest<void>(
-		request.post,
-		null,
-		serviceUrl);
+	return fetch(serviceUrl, { method: "POST" });
 }
 
 /**
  * Retrieves the current status of the hackerspace.
- * @param {ISpaceStatusCallback} callback The callback of the async operation.
  */
-export function getSpaceStatus(): Promise<SpaceStatus> {
-	return doAndParseRequest<SpaceStatus>(
-		request.get,
-		body => wrapWithTry(() => {
-			let status = JSON.parse(body);
-			return fixStatus(status);
-		}),
-		spaceStatusURL);
+export async function getSpaceStatus(): Promise<SpaceStatus> {
+	const res = await fetch(spaceStatusURL);
+	throwIfNotOkay(res);
+	const status = await res.json();
+	return fixStatus(status);
 }
 
 /**
  * Get current power consumption in Watts.
  */
-export function getPowerConsumption(): Promise<PowerConsumption> {
-	return doAndParseRequest<PowerConsumption>(
-		request.get,
-		body => wrapWithTry(() => parsePowerConsumption(body)),
-		powerConsumptionURL);
+export async function getPowerConsumption(): Promise<PowerConsumption> {
+	const res = await fetch(powerConsumptionURL);
+	throwIfNotOkay(res);
+	const consumption = await res.text();
+	return parsePowerConsumption(consumption);
 }
 
 /**
  * Internal method to normalize the return value of the API.
- * @param  {ISpaceStatus} status The original return value.
- * @return {ISpaceStatus}        The normalized result.
+ * @param  {SpaceStatus} status The original return value.
+ * @return {SpaceStatus}        The normalized result.
  */
 function fixStatus(status: SpaceStatus): SpaceStatus {
 	return {
@@ -221,8 +177,7 @@ function getCANUrl(clientName: string, operation: string = ""): string {
 	return `${canBusBase}/${clientName}${operation}`;
 }
 
-function isSuccess(res: any): boolean {
-	if (!res || !res.statusCode)
-		return false;
-	return res.statusCode >= 200 && res.statusCode < 300;
+function throwIfNotOkay(res: Response): never | void {
+	if (!res || !res.ok)
+		throw new Error(`${res.status} ${res.statusText}`);
 }
